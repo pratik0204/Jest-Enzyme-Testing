b322a60aa4367bc14c6455e7fbb0bd044495ea43
Pratik <pchy.393@gmail.com>
2020-05-18 15:50:46 +0530

    simulate click events

--- src/component/button/button.test.js
+++ src/component/button/button.test.js
@@ -20,13 +20,12 @@ describe('SharedButton COmponent', () => {
 
     describe('Renders', () => {
         let wrapper;
-        
+        let mockFunc;
         beforeEach(()=>{
+            mockFunc=jest.fn();
             const props ={
                 buttonText:'Example Button Text',
-                emitEvent:()=>{
-    
-                }
+                emitEvent:mockFunc
             }
             wrapper=shallow(<SharedButon {...props} />);
         });
@@ -34,6 +33,13 @@ describe('SharedButton COmponent', () => {
         it('should render a button', () => {
             const button = findByTestAtrr(wrapper,'buttonComponent');
             expect(button.length).toBe(1);
+        });
+
+        it('should emit callback on click event', () => {
+            const button = findByTestAtrr(wrapper,'buttonComponent');
+            button.simulate('click');
+            const callback = mockFunc.mock.calls.length;
+            expect(callback).toBe(1);
         });
     })
     
